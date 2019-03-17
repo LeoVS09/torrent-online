d507528a2a47538d36d611a821ac57a47dbb60c9
LeoVS09 <leovs09@outlook.com>
2019-03-18 02:24:47 +0300

    fix vs code and typescript bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@
 import HelloWorld from '@/components/HelloWorld.vue' 
 import { Component, Vue } from 'vue-property-decorator'
 import {addTorrent} from '../torrents'
+import WebTorrent from 'webtorrent'
 
 @Component({
   components: {
@@ -20,7 +21,7 @@ export default class Home extends Vue {
   
   public mounted() {
     addTorrent()
-    .then(torrent => {
+    .then((torrent: WebTorrent.Torrent) => {
         // Torrents can contain many files. Let's use the .mp4 file
         const video = torrent.files.find(file => {
           console.log('torrent file', file.name)
